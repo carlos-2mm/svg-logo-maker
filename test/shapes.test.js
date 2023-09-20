a7ae5f41b636c5343f3a784fc8aa23395ac7769c
Carlos Macias <cimm21187@gmail.com>
2023-09-19 23:44:00 -0400

    Modified file shapes.test.js.

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,18 +1,18 @@
 const { Triangle, Circle, Square } = require('../lib/shapes');
 
-test('Triangle SVG', () => {
+test('It should generae a red Triangle SVG', () => {
   const triangle = new Triangle('red');
   expect(triangle.render()).toContain('polygon');
   expect(triangle.render()).toContain('fill="red"');
 });
 
-test('Circle SVG', () => {
+test('It should generate a blue Circle SVG', () => {
   const circle = new Circle('blue');
   expect(circle.render()).toContain('circle');
   expect(circle.render()).toContain('fill="blue"');
 });
 
-test('Square SVG', () => {
+test('It should generate a green Square SVG', () => {
   const square = new Square('green');
   expect(square.render()).toContain('rect');
   expect(square.render()).toContain('fill="green"');
